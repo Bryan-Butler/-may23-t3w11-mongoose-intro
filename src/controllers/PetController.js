f1bb919d4e0325d41168eea029ccce6dfadbdc28
Bryan Butler <butlerbryan00@gmail.com>
2024-01-20 10:07:14 +1000

    controllers
    update and delete

--- src/controllers/PetController.js
+++ src/controllers/PetController.js
@@ -53,11 +53,38 @@ router.post('/', async (request, response) => {
     }
     */
     // error handling via Promise.catch()
-    let result = await Pet.Create(request.body);
+    let result = await Pet.Create(request.body).catch(error => {return error});
 
     response.json({
         pet: result
     });
+})
+
+
+// find a pet by id and update it
+// we need the id but also the attributres to be updated
+router.patch('/:id', async (request, response) =>{
+    let result = await Pet.findByIdAndUpdate(
+        request.params.id,
+        request.body,
+        {
+            returnDocument: 'after',
+            upster: true
+        }
+    ).catch(error => {return error})
+
+    response.json({
+        updatedPet: result
+    })
+})
+
+
+//find pet by id and delete it
+router.delete('/:id', async (request, response) => {
+    await Pet.findByIdAndDelete(request.params.id).catch(error => {return "pet id not found"});
+    response.json({
+        message: "pet deleted"
+    })
 })
 
 module.exports = router 
